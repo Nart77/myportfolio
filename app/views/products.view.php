a9409ca9daa211798876e5053c8ac8f7bbd6eb6f
Nart77 <nartnaghai@gmail.com>
2021-01-14 17:47:51 +0100

    added website logo and changed the way to add image

--- app/views/products.view.php
+++ app/views/products.view.php
@@ -3,18 +3,19 @@
     <h1>Products Page</h1>
 
 
-<h1>Here the form to login</h1>
+
 
 <form method="POST" action="/products">
     <input name="title">title</input>
-    <input name="image">image</input>
+    <input name="image" type="file" class="form-control-file" id="exampleFormControlFile1">
+    <!-- <input name="image">image</input> -->
     <input name="description">Description</input>
     <button type="submit">Submit</button>
 </form>
 <div>
      <?php foreach ($products as $product) : ?>
   <h1> <?= $product->title; ?> </h1>
-   <img src="<?= $product->image; ?>"> 
+   <img src="/public/images/<?= $product->image; ?>"> 
   <p> <?= $product->description; ?> </p>
   <a href="/products/delete?id=<?= $product->id; ?>">delete</a>
 <?php endforeach; ?>
